afd6b435ef03b52a3caba8caea84d7266f8bf564
Ihor Krsnopolskyi <137111710+Jallzy@users.noreply.github.com>
2025-02-17 11:38:31 +0000

    Tic Tac Toe

---
+++ imba/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace imba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTopLeft = new System.Windows.Forms.Button();
+            this.btnTopMiddle = new System.Windows.Forms.Button();
+            this.btnTopRight = new System.Windows.Forms.Button();
+            this.btnMiddleLeft = new System.Windows.Forms.Button();
+            this.btnMiddleMiddle = new System.Windows.Forms.Button();
+            this.btnMiddleRight = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBottomLeft = new System.Windows.Forms.Button();
+            this.btnBottomMiddle = new System.Windows.Forms.Button();
+            this.btnBottomRight = new System.Windows.Forms.Button();
+            this.lblWhoseGo = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTopLeft
+            // 
+            this.btnTopLeft.Location = new System.Drawing.Point(208, 75);
+            this.btnTopLeft.Name = "btnTopLeft";
+            this.btnTopLeft.Size = new System.Drawing.Size(94, 76);
+            this.btnTopLeft.TabIndex = 0;
+            this.btnTopLeft.UseVisualStyleBackColor = true;
+            this.btnTopLeft.Click += new System.EventHandler(this.ButtonClick);
+            this.btnTopLeft.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            // 
+            // btnTopMiddle
+            // 
+            this.btnTopMiddle.Location = new System.Drawing.Point(308, 75);
+            this.btnTopMiddle.Name = "btnTopMiddle";
+            this.btnTopMiddle.Size = new System.Drawing.Size(108, 76);
+            this.btnTopMiddle.TabIndex = 1;
+            this.btnTopMiddle.UseVisualStyleBackColor = true;
+            this.btnTopMiddle.Click += new System.EventHandler(this.ButtonClick);
+            this.btnTopMiddle.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            // 
+            // btnTopRight
+            // 
+            this.btnTopRight.Location = new System.Drawing.Point(423, 75);
+            this.btnTopRight.Name = "btnTopRight";
+            this.btnTopRight.Size = new System.Drawing.Size(107, 76);
+            this.btnTopRight.TabIndex = 2;
+            this.btnTopRight.UseVisualStyleBackColor = true;
+            this.btnTopRight.Click += new System.EventHandler(this.ButtonClick);
+            this.btnTopRight.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            // 
+            // btnMiddleLeft
+            // 
+            this.btnMiddleLeft.Location = new System.Drawing.Point(208, 167);
+            this.btnMiddleLeft.Name = "btnMiddleLeft";
+            this.btnMiddleLeft.Size = new System.Drawing.Size(94, 63);
+            this.btnMiddleLeft.TabIndex = 3;
+            this.btnMiddleLeft.UseVisualStyleBackColor = true;
+            this.btnMiddleLeft.Click += new System.EventHandler(this.ButtonClick);
+            this.btnMiddleLeft.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            // 
+            // btnMiddleMiddle
+            // 
+            this.btnMiddleMiddle.Location = new System.Drawing.Point(309, 167);
+            this.btnMiddleMiddle.Name = "btnMiddleMiddle";
+            this.btnMiddleMiddle.Size = new System.Drawing.Size(107, 63);
+            this.btnMiddleMiddle.TabIndex = 4;
+            this.btnMiddleMiddle.UseVisualStyleBackColor = true;
+            this.btnMiddleMiddle.Click += new System.EventHandler(this.ButtonClick);
+            this.btnMiddleMiddle.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            // 
+            // btnMiddleRight
+            // 
+            this.btnMiddleRight.Location = new System.Drawing.Point(423, 167);
+            this.btnMiddleRight.Name = "btnMiddleRight";
+            this.btnMiddleRight.Size = new System.Drawing.Size(107, 63);
+            this.btnMiddleRight.TabIndex = 5;
+            this.btnMiddleRight.UseVisualStyleBackColor = true;
+            this.btnMiddleRight.Click += new System.EventHandler(this.ButtonClick);
+            this.btnMiddleRight.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(208, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Player1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(441, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 7;
+            // 
+            // btnBottomLeft
+            // 
+            this.btnBottomLeft.Location = new System.Drawing.Point(211, 236);
+            this.btnBottomLeft.Name = "btnBottomLeft";
+            this.btnBottomLeft.Size = new System.Drawing.Size(91, 59);
+            this.btnBottomLeft.TabIndex = 8;
+            this.btnBottomLeft.UseVisualStyleBackColor = true;
+            this.btnBottomLeft.Click += new System.EventHandler(this.ButtonClick);
+            this.btnBottomLeft.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            // 
+            // btnBottomMiddle
+            // 
+            this.btnBottomMiddle.Location = new System.Drawing.Point(308, 236);
+            this.btnBottomMiddle.Name = "btnBottomMiddle";
+            this.btnBottomMiddle.Size = new System.Drawing.Size(108, 59);
+            this.btnBottomMiddle.TabIndex = 9;
+            this.btnBottomMiddle.UseVisualStyleBackColor = true;
+            this.btnBottomMiddle.Click += new System.EventHandler(this.ButtonClick);
+            this.btnBottomMiddle.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            // 
+            // btnBottomRight
+            // 
+            this.btnBottomRight.Location = new System.Drawing.Point(423, 236);
+            this.btnBottomRight.Name = "btnBottomRight";
+            this.btnBottomRight.Size = new System.Drawing.Size(107, 59);
+            this.btnBottomRight.TabIndex = 10;
+            this.btnBottomRight.Text = "‎";
+            this.btnBottomRight.UseVisualStyleBackColor = true;
+            this.btnBottomRight.Click += new System.EventHandler(this.ButtonClick);
+            this.btnBottomRight.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            // 
+            // lblWhoseGo
+            // 
+            this.lblWhoseGo.AutoSize = true;
+            this.lblWhoseGo.Location = new System.Drawing.Point(464, 29);
+            this.lblWhoseGo.Name = "lblWhoseGo";
+            this.lblWhoseGo.Size = new System.Drawing.Size(87, 16);
+            this.lblWhoseGo.TabIndex = 11;
+            this.lblWhoseGo.Text = "WhoseStatus";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(346, 28);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(44, 16);
+            this.lblStatus.TabIndex = 12;
+            this.lblStatus.Text = "Status";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(328, 337);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(390, 101);
+            this.btnReset.TabIndex = 13;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblWhoseGo);
+            this.Controls.Add(this.btnBottomRight);
+            this.Controls.Add(this.btnBottomMiddle);
+            this.Controls.Add(this.btnBottomLeft);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMiddleRight);
+            this.Controls.Add(this.btnMiddleMiddle);
+            this.Controls.Add(this.btnMiddleLeft);
+            this.Controls.Add(this.btnTopRight);
+            this.Controls.Add(this.btnTopMiddle);
+            this.Controls.Add(this.btnTopLeft);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTopLeft;
+        private System.Windows.Forms.Button btnTopMiddle;
+        private System.Windows.Forms.Button btnTopRight;
+        private System.Windows.Forms.Button btnMiddleLeft;
+        private System.Windows.Forms.Button btnMiddleMiddle;
+        private System.Windows.Forms.Button btnMiddleRight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBottomLeft;
+        private System.Windows.Forms.Button btnBottomMiddle;
+        private System.Windows.Forms.Button btnBottomRight;
+        private System.Windows.Forms.Label lblWhoseGo;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ imba/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imba
+{
+    public partial class Form1 : Form
+    {
+        private string currentPlayer = "X";
+        private bool gameActive = true;
+        private Button[,] board;
+        private int playerXScore = 0;
+        private int playerOScore = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            board = new Button[3, 3]
+            {
+                {btnTopLeft,  btnTopMiddle , btnTopRight},
+                {btnMiddleLeft , btnMiddleMiddle , btnMiddleRight } ,
+                {btnBottomLeft , btnBottomMiddle, btnBottomRight }
+            };
+        }
+
+
+
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+if (!gameActive) return;
+
+            Button button = sender as Button;
+            if (button != null && button.Text == "")
+            {
+                button.Text = currentPlayer;
+                CheckForWinner();
+                currentPlayer = (currentPlayer == "X") ? "0" : "X";
+                lblWhoseGo.Text = $"Player{currentPlayer}'s Turn";
+            }
+        }
+
+        private void CheckForWinner()
+        {
+            if (!gameActive) return;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if ((board[i, 0].Text == currentPlayer) &&
+                    (board[i, 1].Text == currentPlayer) &&
+                    (board[i, 2].Text == currentPlayer))
+                {
+                    EndGame($"Player {currentPlayer} Wins!");
+                }
+
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                if ((board[0, i].Text == currentPlayer) &&
+                     (board[1, i].Text == currentPlayer) &&
+                     (board[2, i].Text == currentPlayer))
+                {
+                    EndGame($"Player {currentPlayer} Wins!");
+                }
+            }
+
+            if ((board[0, 0].Text == currentPlayer) &&
+             (board[1, 1].Text == currentPlayer) &&
+             (board[2, 2].Text == currentPlayer))
+            {
+                EndGame($"Player {currentPlayer} Wins!");
+            }
+
+            if ((board[0, 2].Text == currentPlayer) &&
+            (board[1, 1].Text == currentPlayer) &&
+            (board[2, 0].Text == currentPlayer))
+            {
+                EndGame($"Player {currentPlayer} Wins!");
+            }
+
+            bool draw = true;
+            foreach (Button b in board)
+            {
+                if (b.Text == "")
+                {
+                    draw = false;
+                }
+
+            }
+            if (draw)
+            {
+                EndGame($"It's a draw");
+            }
+
+        }
+
+        private void EndGame(string result)
+        {
+            gameActive = false;
+            if (result.Contains("X")) { playerXScore++; }
+            if (result.Contains("0")) { playerOScore++; }
+
+            lblStatus.Text = result + $"     Player X : {playerXScore}   || Player O:   {playerOScore}";
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            foreach (Button b in board)
+            {
+                b.Text = "";
+            }
+            gameActive  = true;
+            currentPlayer = "X";
+            lblWhoseGo.Text = "X to start!";
+            btnBottomLeft.Focus();
+        }
+
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode){
+                case Keys.D1:
+                    btnTopLeft.PerformClick();
+                    break;
+                    case Keys.D2:
+                    btnTopMiddle.PerformClick();
+                    break;
+                case Keys.D3:
+                    btnTopRight.PerformClick();
+                    break;
+                case Keys.D4:
+                    btnMiddleLeft.PerformClick();
+                    break;
+                case Keys.D5:
+                    btnMiddleMiddle.PerformClick();
+                    break;
+                case Keys.D6:
+                    btnMiddleRight.PerformClick();
+                    break;
+                    case Keys.D7:
+                    btnBottomLeft.PerformClick();
+                    break;
+                    case Keys.D8:
+                    btnBottomMiddle.PerformClick();
+                    break;
+                    case Keys.D9:
+                    btnBottomLeft.PerformClick();
+                    break;
+
+
+            }
+        }
+    }
+}
